ed78e247731371c05f73d14621b7474424c5a5e6
蹇佳成 <3094098662@qq.com>
2026-03-10 23:49:01 +0800

    添加了网络插件

---
+++ FantasyBeastStories/Assets/Sprites/Photon/Launcher.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("Connected to master");
+    }
+}
