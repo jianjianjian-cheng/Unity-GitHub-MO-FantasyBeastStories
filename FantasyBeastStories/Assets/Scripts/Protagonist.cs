19b72c338c46183eb8ef4bc51d1cb190033c043c
蹇佳成 <3094098662@qq.com>
2026-03-10 22:33:10 +0800

    添加了一些资源

--- FantasyBeastStories/Assets/Scripts/Protagonist.cs
+++ FantasyBeastStories/Assets/Scripts/Protagonist.cs
@@ -9,6 +9,9 @@ namespace Player
         [Header("角色变量设置")]
         private bool isRun;
         private bool isRight;
+        private bool isJump;
+        private bool isGround;
+        [SerializeField] private float groundDistance = 0.2f; // 地面检测距离
         [Header("移动设置")]
         [SerializeField] private float moveSpeed = 5f; // 移动速度
 
@@ -30,12 +33,13 @@ namespace Player
             }
             rb.useGravity = true; // 启用重力
         }
-
         // Update is called once per frame
         void Update()
         {
             // 获取输入
             HandleInput();
+            // 检查是否在地面
+            CheckIsGround();
         }
 
         void FixedUpdate()
@@ -68,6 +72,12 @@ namespace Player
             {
                 spriteRenderer.flipX = !isRight;
             }
+
+            // 跳跃输入
+            if (Input.GetKeyDown(KeyCode.Space) && isGround && !isJump)
+            {
+                Jump();
+            }
         }
 
         /// <summary>
@@ -84,6 +94,36 @@ namespace Player
 
             // 应用速度
             rb.velocity = moveVelocity;
+        }
+
+        //跳跃方法
+        private void Jump()
+        {
+            isJump = true;
+            animator.SetBool("isJump", isJump);
+            // 跳跃力
+            rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+            Invoke(nameof(ResetIsJump), 1.2f);
+        }
+
+        private void ResetIsJump()
+        {
+            isJump = false;
+            animator.SetBool("isJump", isJump);
+        }
+
+        private void CheckIsGround()
+        {
+            // 检测是否 grounded
+            isGround = Physics.Raycast(transform.position, Vector3.down, groundDistance);
+            animator.SetBool("isGround", isGround);
+        }
+
+        void OnDrawGizmos()
+        {
+            // 绘制角色与地面距离
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundDistance);
         }
     }
 }
