d607fe1a07c78e02ec226527c96e23ef8d91b67a
蹇佳成 <3094098662@qq.com>
2026-03-14 16:31:56 +0800

    更改素材

--- FantasyBeastStories/Assets/Scripts/Charactors/PlayerController.cs
+++ FantasyBeastStories/Assets/Scripts/Charactors/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Player
+namespace Charactors
 {
     public class PlayerController : MonoBehaviour
     {
